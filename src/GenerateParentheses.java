2131f60cfb1e1ed92fee30e5103322ae54839289
BenjaminSong90 <justsonglang@gmail.com>
2019-12-15 22:56:59 +0800

    create GenerateParentheses

---
+++ src/GenerateParentheses.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParentheses {
+    public static void main(String[] a) {
+       List<String> result = new GenerateParentheses().generateParenthesis(2);
+
+       for (String s : result){
+           System.out.println(s);
+       }
+    }
+
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList();
+        backtrack(ans, "", 0, 0, n);
+        return ans;
+    }
+
+    public void backtrack(List<String> ans, String cur, int open, int close, int max){
+        if (cur.length() == max * 2) {
+            ans.add(cur);
+            return;
+        }
+
+        if (open < max)
+            backtrack(ans, cur+"(", open+1, close, max);
+        if (close < open)
+            backtrack(ans, cur+")", open, close+1, max);
+    }
+}
