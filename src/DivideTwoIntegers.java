9832af582c44db9a1c288d9764a338b86137f179
SongLang <justsonglang@gmail.com>
2019-12-17 03:05:06 +0000

    Create DivideTwoIntegers.java

---
+++ src/DivideTwoIntegers.java
@@ -0,0 +1,30 @@
+public class DivideTwoIntegers {
+    public static void main(String[] a) {
+        System.out.println(new DivideTwoIntegers().divide(10,-3));
+    }
+    public int divide(int dividend, int divisor) {
+        boolean sign = (dividend > 0) ^ (divisor > 0);
+        int result = 0;
+        if(dividend>0) {
+            dividend = -dividend;
+        }
+        if(divisor>0) divisor = -divisor;
+        while(dividend <= divisor) {
+            int temp_result = -1;
+            int temp_divisor = divisor;
+            while(dividend <= (temp_divisor << 1)) {
+                if(temp_divisor <= (Integer.MIN_VALUE >> 1))break;
+                temp_result = temp_result << 1;
+                temp_divisor = temp_divisor << 1;
+            }
+            dividend = dividend - temp_divisor;
+            result += temp_result;
+        }
+        if(!sign) {
+            if(result <= Integer.MIN_VALUE) return Integer.MAX_VALUE;
+            result = - result;
+        }
+        return result;
+    }
+
+}
