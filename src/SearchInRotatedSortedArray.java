4aab5e5b4cf198fb5c939a7a5a82f1ed0c13fe7b
SongLang <justsonglang@gmail.com>
2019-12-19 10:39:31 +0800

    Create SearchInRotatedSortedArray.java

---
+++ src/SearchInRotatedSortedArray.java
@@ -0,0 +1,23 @@
+
+public class SearchInRotatedSortedArray {
+    public static void main(String[] a) {
+        int[] nums = {4,5,6,7,0,1,2};
+        System.out.println(new SearchInRotatedSortedArray().search(nums, 5));
+    }
+
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] < nums[right]) {
+                if (nums[mid] < target && nums[right] >= target) left = mid + 1;
+                else right = mid - 1;
+            } else {
+                if (nums[left] <= target && nums[mid] > target) right = mid - 1;
+                else left = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
