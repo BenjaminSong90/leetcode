9166e7c977e73550d6f956431bd195655dc7366f
BenjaminSong90 <justsonglang@gmail.com>
2020-02-16 14:30:41 +0800

    create DecodeWays91.java

---
+++ src/DecodeWays91.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class DecodeWays91 {
+    public static void main(String[] a) {
+        System.out.println(new DecodeWays91().numDecodings("222202"));
+    }
+
+
+    public int numDecodings(String s) {
+        int[] dp = new int[s.length()];
+        int pre = s.charAt(0) - '0';
+        if(pre == 0) return 0;
+        dp[0] = 1;
+        for(int i=1; i<s.length(); i++){
+            int cur = s.charAt(i) - '0';
+            if(cur==0){
+
+                if(pre==0 || pre>2) break;
+
+                if(i>1) dp[i] = dp[i-2];
+                else dp[i] = 1;
+            }
+            else{
+                if(pre!=0 && pre*10+cur<=26){
+
+                    if(i>1) dp[i] = dp[i-1]+dp[i-2];
+                    else dp[i] = dp[i-1] + 1;
+                }
+                else dp[i] = dp[i-1];
+            }
+
+            pre = cur;
+        }
+
+        return dp[s.length()-1];
+
+    }
+
+
+
+
+
+
+}
