811bec394746c65537ffef555cd95f9bf0ebf310
BenjaminSong90 <justsonglang@gmail.com>
2021-09-23 23:40:51 +0800

    _650_2KeysKeyboard 优化

--- src/_650_2KeysKeyboard.java
+++ src/_650_2KeysKeyboard.java
@@ -3,46 +3,42 @@ import java.util.Map;
 
 public class _650_2KeysKeyboard {
 
-    public static void main(String[] args){
-        System.out.println("result>>>>"+new _650_2KeysKeyboard().minSteps(3));
+    public static void main(String[] args) {
+        System.out.println("result>>>>" + new _650_2KeysKeyboard().minSteps(1));
     }
+
     public int minSteps(int n) {
-        return step(n,1,0,0,0);
+        return step(n, 1, 0, 0);
     }
 
     /**
-     *
-     * @param tag 0 复制全部， 1粘贴
+     * @param tag
      * @param currentCount
      * @param selectCount
-     * @param action
      * @param step
      * @return
      */
-    public int step(int tag, int currentCount, int selectCount, int action, int step){
-        if(tag == currentCount) return step;
-        if(selectCount > tag || currentCount > tag) return -1;
+    public int step(int tag, int currentCount, int selectCount, int step) {
+        if (tag == currentCount) return step;
+        if (selectCount > tag || currentCount > tag) return -1;
         int s1 = -1;
         int s2 = -1;
-        if(action == 0){
-            if(currentCount != selectCount){
-                s1 = step(tag, currentCount, currentCount,0,step+1);
-            }
-            s2 = step(tag, currentCount, currentCount,1,step+1);
 
-        } else {
-            s1 = step(tag, currentCount+selectCount, selectCount,0,step+1);
-            s2 = step(tag, currentCount+selectCount, selectCount,1,step+1);
+        if (currentCount != selectCount) {
+            s1 = step(tag, currentCount, currentCount, step + 1);
         }
 
-        if(s1 == -1 && s2 == -1){
+        if (selectCount != 0)
+            s2 = step(tag, currentCount + selectCount, selectCount, step + 1);
+
+        if (s1 == -1 && s2 == -1) {
             return -1;
-        } else if(s1 == -1){
+        } else if (s1 == -1) {
             return s2;
-        } else if(s2 == -1){
+        } else if (s2 == -1) {
             return s1;
         }
-        return Math.min(s1,s2);
+        return Math.min(s1, s2);
 
     }
 
